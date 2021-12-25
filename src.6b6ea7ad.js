871ce4f71bb3e6aac63bcf046dda87bfcbacccff
GrygoriyChernish <63448238+GrygoriyChernish@users.noreply.github.com>
2021-12-25 11:10:11 +0000

    Deploying to gh-pages from @ GrygoriyChernish/fson45-group-05-06@ce75771195c956400e12fb61bb9940958ab07069 🚀

--- src.6b6ea7ad.js
+++ src.6b6ea7ad.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.f6ca2453.js.map
+//# sourceMappingURL=/parcel-project-template/src.6b6ea7ad.js.map
